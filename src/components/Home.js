c50589bec8fa49aba18370726036349f440f555e
William Yu <wy427@nyu.edu>
2022-03-06 20:20:31 -0500

    navbar and header created

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomeStyles.css";
+import Navbar from "./Navbar";
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
